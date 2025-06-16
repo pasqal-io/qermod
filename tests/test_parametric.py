e903d86c15fc09d84d8f9fdc8b74bbd36fe47e75
Charles MOUSSA <charles.moussa@nl-psq-031.local>
2025-06-16 16:43:26 +0200

    test parametric

--- tests/test_parametric.py
+++ tests/test_parametric.py
@@ -1,14 +1,23 @@
 from __future__ import annotations
 
-import pytest
-
-from qermod import Bitflip
 from qadence.parameters import Parameter
 
-def test_parametric() -> None:
+from qermod import Bitflip, IndependentReadout
 
-    noise = Bitflip(error_definition=Parameter('p'))
-    assert not noise.error_definition.is_number
+
+def test_parametric_digital() -> None:
+
+    noise = Bitflip(error_definition=Parameter("p"))
+    assert not noise.error_definition.is_number  # type: ignore[union-attr]
 
     noise = Bitflip(error_definition=Parameter(0.1))
-    assert noise.error_definition.is_number
+    assert noise.error_definition == 0.1
+
+
+def test_parametric_readout() -> None:
+
+    noise = IndependentReadout(error_definition=Parameter("p"))
+    assert not noise.error_definition.is_number  # type: ignore[union-attr]
+
+    noise = IndependentReadout(error_definition=Parameter(0.1))
+    assert noise.error_definition == 0.1
